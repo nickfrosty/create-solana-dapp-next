8410a822376b87399c5b7d4562db46ea3cf271a7
nickfrosty <nfrostbutter@gmail.com>
2023-12-12 10:25:11 -0500

    fix: dist dir

--- web/next.config.js
+++ web/next.config.js
@@ -7,6 +7,7 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  distDir: '../../dist/web',
   webpack: (config) => {
     config.externals = [
       ...(config.externals || []),
